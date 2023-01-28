70492155e124a2f5bb6125b1069ad5a3333e2255
CHN-Rohith <chn-rohith@CHN-XGC4JQ6DC9.local>
2023-01-28 22:45:40 +0530

    add form for new bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,11 @@
 class BookmarksController < ApplicationController
   def index
   end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :bookmarks, only: [:index]
+  resources :bookmarks, only: [:index, :new]
 end
