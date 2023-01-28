150c0fac51b491b8c01db83110535f07945e4bf4
CHN-Rohith <chn-rohith@CHN-XGC4JQ6DC9.local>
2023-01-29 00:31:00 +0530

    Create new bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,5 +8,19 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      flash[:notice] = "Successfully created bookmark."
+      redirect_to bookmarks_path
+    else
+      flash[:notice] = "Error in creating bookmark."
+      render 'new', :status => :unprocessable_entity
+    end
   end
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:title, :description, :address, :imageURL)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :bookmarks, only: [:index, :new]
+  resources :bookmarks, only: [:index, :new, :create]
 end
