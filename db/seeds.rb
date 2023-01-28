6cc64db8307db6fccacec87ae971d6581aba407b
CHN-Rohith <chn-rohith@CHN-XGC4JQ6DC9.local>
2023-01-28 23:28:22 +0530

    Fetch all bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Bookmark.create([
+    {
+        title: "Barbeque Nation",                                     
+        imageURL: "https://studiousguy.com/wp-content/uploads/2021/02/Barbeque-Nation.jpeg",
+        address: "No 39, Star City Serviced Apartment, Bazullah Rd, near The Grand Sweets, T. Nagar, Chennai, Tamil Nadu 600017",
+        description: "Starters, Pasta, Dessets, Mocktails", 
+    },
+    {
+        title: "Savoury SeaShell Restaurant",                                     
+        imageURL: "http://www.seashellchennai.com/images/seashellimage(a).png",
+        address: "No 39, Star City Serviced Apartment, Bazullah Rd, near The Grand Sweets, T. Nagar, Chennai, Tamil Nadu 600017",
+        description: "Starters, Pasta, Dessets, Mocktails", 
+    }
+])
